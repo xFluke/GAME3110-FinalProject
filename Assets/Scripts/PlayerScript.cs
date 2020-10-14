be8cb93a04cbf17d82a6b781684780587594691c
xFluke <mikoman1124@gmail.com>
2020-10-14 18:54:02 -0400

    Imported Assets and Setup Basic Scene

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 6.0f;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
