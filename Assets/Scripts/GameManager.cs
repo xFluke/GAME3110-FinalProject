7f024b8ccefe4bfa8dd73eb508a8c4bcee9e14e8
xFluke <mikoman1124@gmail.com>
2020-10-14 19:12:07 -0400

    After 5 seconds, customer spawns and walks in

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject customerSpawnPoint;
+    public GameObject customerPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SpawnCustomer", 5f);
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class GameManager : MonoBehaviour
     }
 
     void SpawnCustomer() {
-
+        GameObject customer = Instantiate(customerPrefab, customerSpawnPoint.transform.position, Quaternion.identity);
+        customer.GetComponent<NavMeshAgent>().SetDestination(new Vector3(-9f, 1.5f, 0.8f));
     }
 }
