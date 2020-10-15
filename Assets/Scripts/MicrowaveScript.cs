7f8047340e11518892661ccd68dd0e17c73cd4ef
xFluke <mikoman1124@gmail.com>
2020-10-14 20:31:27 -0400

    Microwave Opens within Proximity

---
+++ Assets/Scripts/MicrowaveScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrowaveScript : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("yuh");
+            gameObject.GetComponentInParent<Animator>().SetBool("playerInRange", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            gameObject.GetComponentInParent<Animator>().SetBool("playerInRange", false);
+        }
+    }
+}
