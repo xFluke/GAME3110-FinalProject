861b4a97cd652edf9d3639e4ac65d3b623440ccb
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-10-16 18:30:46 +0800

    Start scene and login panel added

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject quitButton;
+    public GameObject LoginPanel;
+
+    public TMP_Text usernameInput;
+    public TMP_Text passwordInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayButtonPressed()
+    {
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        LoginPanel.SetActive(true);
+        //Debug.Log("Play!");
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void OnLoginButtonPressed()
+    {
+        if (usernameInput.text.Length > 1 && passwordInput.text.Length > 1)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        Debug.Log("Username:" + usernameInput.text);
+        Debug.Log("Password:" + passwordInput.text);
+    }
+}
