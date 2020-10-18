edb08ec92402bc44359710c58b8b16954f6bb252
Tariv093 <tariv093@gmail.com>
2020-10-18 02:12:42 -0400

    steak steak

---
+++ Assets/Scripts/CustomerScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class CustomerScript : MonoBehaviour
+{
+    private GameObject player;
+    public bool foodReq;
+    public GameObject steak;
+    private NavMeshAgent nma;
+    public GameObject delObj;
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        nma = GetComponent<NavMeshAgent>();
+        
+        InvokeRepeating("CheckDistance", 0.5f,0.5f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+      //  Debug.Log(nma.remainingDistance);
+       
+        if (foodReq == true)
+        FoodRequest();
+    }
+    void CheckDistance()
+    {
+        if (nma.remainingDistance < 0.5f)
+        {
+            foodReq = true;
+            CancelInvoke("CheckDistance");
+        }
+       
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+           
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (Input.GetKey(KeyCode.E) && player.GetComponent<PlayerScript>().itemInHand == PlayerScript.ItemInHand.COOKED_STEAK) {
+                Destroy(other.transform.GetChild(0).gameObject);
+                player.GetComponent<PlayerScript>().itemInHand = PlayerScript.ItemInHand.EMPTY;
+
+                StartCoroutine(DeliverFood());                               
+            }
+        }
+    }
+
+    IEnumerator DeliverFood() {
+        Debug.Log("Sending Food");
+        //Vector3 pos = new Vector3(-25f, transform.position.y, transform.position.z);
+        transform.GetComponent<NavMeshAgent>().SetDestination(transform.position + new Vector3(-16f, 0, 0));
+        yield return new WaitForSeconds(2);
+        Destroy(transform.gameObject);
+    }
+
+   void FoodRequest()
+    {
+       
+        Instantiate(steak, new Vector3(transform.position.x, transform.position.y + 2 , transform.position.z),Quaternion.identity);
+        foodReq = false;
+        Debug.Log("requesting food");
+        
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,12 +19,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+       
+        if (customerList.Count > 5) {
+            CancelInvoke("SpawnCustomer");
+        }
+>>>>>>> Stashed changes
     }
 
     void SpawnCustomer() {
         GameObject customer = Instantiate(customerPrefab, customerSpawnPoint.transform.position, Quaternion.identity);
         customerList.Add(customer);
+<<<<<<< Updated upstream
         if(customerList.Count <= 1)
         customer.GetComponent<NavMeshAgent>().SetDestination(new Vector3(-9f, 1.5f, 0.8f));
         else
@@ -32,6 +40,14 @@ public class GameManager : MonoBehaviour
             {
                 customerList[i].GetComponent<NavMeshAgent>().SetDestination(new Vector3(-9f , 1.5f, (0.8f + i*2)));   
             }
+=======
+      
+        customer.GetComponent<NavMeshAgent>().SetDestination(customerFinalDestination + new Vector3(0, 0, (customerList.Count - 1) * 2));
+       // Debug.Log(customer.GetComponent<NavMeshAgent>().remainingDistance);
+//if (customer.GetComponent<NavMeshAgent>().remainingDistance < 0.5f)
+       // customer.GetComponent<NavMeshAgent>().
+        //customer.GetComponent<CustomerScript>().foodReq = true;
+>>>>>>> Stashed changes
     }
 
 }
